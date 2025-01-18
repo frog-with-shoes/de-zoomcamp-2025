c997edc30b63e29fc2cca52a382c09f9f5a73f49
frog-with-shoes <froggy.at.the.market@gmail.com>
2025-01-18 11:52:08 -0500

    Ingesting taxi zone dataset, did some sql queries(join, groupby, order by, sub queries, casting, check for null.

---
+++ week_1_basics_n_setup/2_docker_sql/ingesting_zones.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import argparse
+
+from sqlalchemy import create_engine
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    # url = params.url
+
+    csv_name = "taxi_zone_lookup.csv.1"
+
+    df = pd.read_csv(csv_name)
+    print(df)
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    # parser.add_argument('--url', required=True, help='url of the csv file')
+
+    args = parser.parse_args()
+
+    main(args)
+
