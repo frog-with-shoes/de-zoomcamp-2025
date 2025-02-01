190189988bc085c1f3d1d6b6e321bd309571e512
frog-with-shoes <froggy.at.the.market@gmail.com>
2025-01-31 22:52:10 -0500

    hw week 2 submission.

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -7,4 +7,25 @@ print(sys.argv)
 day = sys.argv[1]
 # Some fancy stuff
 
-print('job finished succ', day)
+print('job finished succes', day)
+
+
+# docker run -it \
+#   -e POSTGRES_USER="root" \
+#   -e POSTGRES_PASSWORD="root" \
+#   -e POSTGRES_DB="ny_taxi" \
+#   -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+#   -p 5432:5432 \
+#   --network=pg-network \
+#   --name pg-database \
+#   postgres:13
+
+
+
+# docker run -it \
+#   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+#   -e PGADMIN_DEFAULT_PASSWORD="root" \
+#   -p 8080:80 \
+#   --network=pg-network \
+#   --name pgadmin-2 \
+#   dpage/pgadmin4
