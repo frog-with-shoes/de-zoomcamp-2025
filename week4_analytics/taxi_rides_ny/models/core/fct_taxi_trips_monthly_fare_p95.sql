d1440efa41a052b8edb2cc29feb6b6a77698fad4
frog-with-shoes <froggy.at.the.market@gmail.com>
2025-03-07 14:27:56 +0000

    Troubleshooted by creating a new table, the schema for green taxi needs to be revised.

--- week4_analytics/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ week4_analytics/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -3,8 +3,18 @@
         materialized='table'
     )
 }}
-
+with fact_trips_table as (
     select  *
     from {{ref('fact_trips')}}
     where fare_amount > 0 and trip_distance > 0 and payment_type_description in ('Cash', 'Credit card')
+)
 
+select 
+service_type, 
+year, 
+month, 
+fare_amount,
+PERCENTILE_CONT(fare_amount, 0.97) over(partition by service_type, year, month) as percentile_97_fare_amount,
+PERCENTILE_CONT(fare_amount, 0.95) over(partition by service_type, year, month) as percentile_95_fare_amount,
+PERCENTILE_CONT(fare_amount, 0.90) over(partition by service_type, year, month) as percentile_90_fare_amount,
+from fact_trips_table
